e739f5ce52381a58cf6d17c6bc93cb704efbaec4
Rijckaert Tom <200082@CCM-Asty-Moulin.local>
2023-03-30 15:58:56 +0200

    création du projet en c#

---
+++ Jeu-De-Vie/Jeu-De-Vie/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jeu_De_Vie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
