98763e0b8935442b86717e32b4e8b5faf3ada8be
Rijckaert Tom <200082@CCM-Asty-Moulin.local>
2023-04-06 16:12:02 +0200

    fin programme principal

--- Jeu-De-Vie/Jeu-De-Vie/MethodeDuProjet.cs
+++ Jeu-De-Vie/Jeu-De-Vie/MethodeDuProjet.cs
@@ -2,8 +2,142 @@
 
 public struct MethodeDuProjet
 {
-	public MethodeDuProjet()
-	{
+    public void GenMatrice(int _nbLigne, int _nbColonne, out bool[,] _matrice)
+    {
+        // instantiation de la matrice tabAlea
+        _matrice = new bool[_nbLigne, _nbColonne];
+        // instantiation de la variable random
+        Random random = new Random();
+        // boucle pour passer dans les lignes de la matrice
+        for (int ligne = 0; ligne < _nbLigne - 1; ligne++)
+        {
+            // boucle pour passer dans les colonnes de la matrice
+            for (int colonne = 0; colonne < _nbColonne - 1; colonne++)
+            {
+                // mise des données alléatoires true ou false si la cellule est vivante ou morte
+                _matrice[ligne, colonne] = random.Next(2) == 1;
+            }
+        }
+    }
+    //morceau de programme pour concaténer (afficher) la matrice
+    public void ShowMatrice(bool[,] _matrice, out string _matriceChaine)
+    {
+        _matriceChaine = "";
+        // boucle pour se balades dans les lignes de la matrice
+        for (int ligne = 0; ligne < _matrice.GetLength(0) - 1; ligne++)
+        {
+            // boucle pour se balader dans les colonnes de la matrice
+            for (int colonne = 0; colonne < _matrice.GetLength(1) - 1; colonne++)
+            {
+                // mise des données de la matrice dans la variable string stringTab
+                if (_matrice[ligne, colonne] == true)
+                {
+                    _matriceChaine += "O";
+                }
+                else
+                {
+                    _matriceChaine += "-";
+                }
+            }
+            // passage à la ligne
+            _matriceChaine += "\n";
+        }
+    }
 
-	}
+    public void UpdateMatrice(bool[,] _matrice)
+    {
+        bool[,] nouvelleMatrice = new bool[_matrice.GetLength(0), _matrice.GetLength(1)];
+
+        for (int i = 0; i < _matrice.GetLength(0); i++)
+        {
+            for (int j = 0; j < _matrice.GetLength(1); j++)
+            {
+                int nbVoisins = CountVoisins(_matrice, i, j);
+
+                if (_matrice[i, j])
+                {
+                    // Si la cellule est vivante et a deux ou trois voisins, elle reste vivante, sinon elle meurt.
+                    nouvelleMatrice[i, j] = (nbVoisins == 2 || nbVoisins == 3);
+                }
+                else
+                {
+                    // Si la cellule est morte et a exactement trois voisins, elle devient vivante.
+                    nouvelleMatrice[i, j] = (nbVoisins == 3);
+                }
+            }
+        }
+
+        // Mettre à jour la matrice
+        for (int i = 0; i < _matrice.GetLength(0); i++)
+        {
+            for (int j = 0; j < _matrice.GetLength(1); j++)
+            {
+                _matrice[i, j] = nouvelleMatrice[i, j];
+            }
+        }
+    }
+
+    public int CountVoisins(bool[,] _matrice, int x, int y)
+    {
+        int count = 0;
+
+        for (int i = -1; i <= 1; i++)
+        {
+            for (int j = -1; j <= 1; j++)
+            {
+                if (i == 0 && j == 0) continue;
+
+                int row = x + i;
+                int col = y + j;
+
+                if (row < 0 || col < 0) continue;
+                if (row >= _matrice.GetLength(0) || col >= _matrice.GetLength(1)) continue;
+
+                if (_matrice[row, col])
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public void EtapeSuivante(bool[,] _matrice, out bool[,] _nouvelleMatrice)
+    {
+        int nbLignes = _matrice.GetLength(0);
+        int nbColonnes = _matrice.GetLength(1);
+        _nouvelleMatrice = new bool[nbLignes, nbColonnes];
+
+        for (int i = 0; i < nbLignes; i++)
+        {
+            for (int j = 0; j < nbColonnes; j++)
+            {
+                int nbVoisins = CountVoisins(_matrice, i, j);
+
+                if (_matrice[i, j])
+                {
+                    // Si la cellule est vivante et a deux ou trois voisins, elle reste vivante, sinon elle meurt.
+                    _nouvelleMatrice[i, j] = (nbVoisins == 2 || nbVoisins == 3);
+                }
+                else
+                {
+                    // Si la cellule est morte et a exactement trois voisins, elle devient vivante.
+                    _nouvelleMatrice[i, j] = (nbVoisins == 3);
+                }
+            }
+        }
+
+        // Mettre à jour la matrice
+        for (int i = 0; i < nbLignes; i++)
+        {
+            for (int j = 0; j < nbColonnes; j++)
+            {
+                _matrice[i, j] = _nouvelleMatrice[i, j];
+            }
+        }
+    }
+
 }
+
+
--- Jeu-De-Vie/Jeu-De-Vie/Program.cs
+++ Jeu-De-Vie/Jeu-De-Vie/Program.cs
@@ -6,9 +6,46 @@ namespace Jeu_De_Vie
     {
         static void Main(string[] args)
         {
+            Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+            Console.SetBufferSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+            Console.WindowWidth = Console.LargestWindowWidth;
+            Console.WindowHeight = Console.LargestWindowHeight;
+            Console.BufferWidth = Console.LargestWindowWidth;
+            Console.BufferHeight = Console.LargestWindowHeight;
+
             MethodeDuProjet mesOutils = new MethodeDuProjet();
 
-        }   
-         
+            while (true)
+            {
+                string textToEnter = "Bienvenue dans le jeu de la vie";
+                Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (textToEnter.Length / 2)) + "}", textToEnter));
+                Console.WriteLine("Taper 1 pour commencer le jeu de la vie en mode alléatoire : ");
+                int lancer = int.Parse(Console.ReadLine());
+                if (lancer == 1)
+                {
+                    while (true)
+                    {
+                        // appel de la méthode GenMatrice pour générer la matrice de départ
+                        int _nbLigne = 100;
+                        int _nbColonne = 100;
+                        mesOutils.GenMatrice(_nbLigne, _nbColonne, out bool[,] _matrice);
+                        mesOutils.ShowMatrice(_matrice, out string _matriceChaine);
+
+                        System.Threading.Thread.Sleep(100);
+
+                        // appel de la méthode EtapeSuivante pour obtenir la nouvelle matrice
+                        mesOutils.EtapeSuivante(_matrice, out bool[,] _nouvelleMatrice);
+
+                        Console.Clear();
+
+
+                        // affichage de la nouvelle matrice
+                        mesOutils.ShowMatrice(_nouvelleMatrice, out string _nouvelleMatriceChaine);
+                        Console.WriteLine("Nouvelle matrice : \n" + _nouvelleMatriceChaine);
+
+                    }
+                }
+            }
+        }
     }
 }
